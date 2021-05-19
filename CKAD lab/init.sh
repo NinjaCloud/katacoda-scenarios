9dd7a3081dbd9c4914dbaaeff2879d76764094c6
ninad <ninad@unigps.in>
2021-05-19 19:09:39 +0530

    CKAD lab

--- CKAD lab/init.sh
+++ CKAD lab/init.sh
@@ -1,6 +1,16 @@
 #!/usr/bin/env bash
-mkdir ~/test
-pip install --upgrade pip
-pip install --upgrade bdworkbench
+mkdir ~/CKAD
+sudo yum -y update
+sudo yum -y install epel-release
+sudo yum -y install libvirt qemu-kvm virt-install virt-top libguestfs-tools bridge-utils
+sudo systemctl start libvirtd
+sudo systemctl enable libvirtd
+systemctl status libvirtd
+sudo usermod -a -G libvirt $(whoami)
+sudo vi /etc/libvirt/libvirtd.conf
+sudo systemctl restart libvirtd.service
+wget https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
+chmod +x minikube-linux-amd64
+sudo mv minikube-linux-amd64 /usr/local/bin/minikube
 cd
 clear
